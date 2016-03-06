157aa28524aa57e4ffd578ff07c13a4ae855f7ee
Kent Lee <kentdlee@gmail.com>
2016-03-06 17:42:53 -0600

    Start of Lesson 2

--- draw.py
+++ draw.py
@@ -1,2 +1,33 @@
 from turtle import *
 
+t = Turtle()
+screen = t.getscreen()
+
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+
+t.penup()
+t.goto(-200,200)
+t.pendown()
+
+t.color("red")
+t.fillcolor("blue")
+
+t.begin_fill()
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.end_fill()
+
+
+
+
+screen.exitonclick()
+
