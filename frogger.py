dc1cbac229f06d5e7411dc17dc91b2212b607028
Kent Lee <kentdlee@gmail.com>
2016-03-08 22:46:53 -0600

    no message

--- frogger.py
+++ frogger.py
@@ -9,7 +9,34 @@ class FroggerApplication(tkinter.Frame):
         self.buildWindow()
         
     def buildWindow(self):
-        pass
+        self.master.title("Frogger!")
+        canvas = tkinter.Canvas(self,width=600,height=600)
+        canvas.pack(side=tkinter.LEFT)
+        
+        turtle = RawTurtle(canvas)
+        
+        turtle.color("blue")
+        turtle.fillcolor("blue")
+        turtle.begin_fill()
+        for distance in [350,200,700,200,350]:
+            turtle.forward(distance)
+            turtle.left(90)
+            
+        turtle.end_fill()
+        turtle.right(90)
+        turtle.color("grey")
+        turtle.fillcolor("grey")
+        turtle.begin_fill()
+        for distance in [350,200,700,200,350]:
+            turtle.forward(distance)
+            turtle.right(90)
+            
+        turtle.end_fill() 
+        turtle.ht()
+            
+        
+            
+        
     
     
 def main():
