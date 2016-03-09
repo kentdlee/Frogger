17599e8bf056c946ab486b20a2c4dae564e73635
Kent Lee <kentdlee@gmail.com>
2016-03-08 21:42:27 -0600

    no message

--- draw.py
+++ draw.py
@@ -1,57 +1,81 @@
 from turtle import *
 
-def drawSquare(t, sideLength):
+# f(x) = x * x
+
+def drawSquare(turtle, x, y, sideLength, fillcolor):
+    turtle.penup()
+    turtle.goto(x,y)
+    turtle.pendown()
+    turtle.fillcolor(fillcolor) 
+    turtle.begin_fill()    
     for i in range(4):
-        t.forward(sideLength)
-        t.right(90)    
-
-def drawTriangle(t, sideLength):
-    for i in range(3):
-        t.forward(sideLength)
-        t.left(120)    
-
-def drawHexagon(t, sideLength):
-    for i in range(6):
-        t.forward(sideLength)
-        t.left(60)    
-
-def drawPolygon(t, sideLength, numberOfSides, color):
-    t.fillcolor(color)
-    t.begin_fill()
-    for i in range(numberOfSides):
-        t.forward(sideLength)
-        angle = 360 / numberOfSides
-        t.left(angle)
-    t.end_fill()
+        turtle.forward(sideLength)
+        turtle.right(90)  
+    turtle.end_fill()
     
+def drawTriangle(turtle, sideLength):
+    for i in range(3):
+        turtle.forward(sideLength)
+        turtle.left(120)  
+        
+def drawHexagon(turtle, sideLength):
+    for i in range(6):
+        turtle.forward(sideLength)
+        turtle.left(60)
+        
+def drawPolygon(turtle, x, y, sideLength, numberOfSides, fillcolor):
+    turtle.penup()
+    turtle.goto(x,y)
+    turtle.pendown()
+    
+    turtle.fillcolor(fillcolor)
+    turtle.begin_fill()
+    angle = 360 / numberOfSides
+    for i in range(numberOfSides):
+        turtle.forward(sideLength)
+    
+        turtle.left(angle)
+    turtle.end_fill()    
     
 def main():
-    t = Turtle()
-    screen = t.getscreen()
+    turtle = Turtle()
+    screen = turtle.getscreen()
     
-    drawPolygon(t, 1, 200, "pink")
-    
-    drawSquare(t,100)   
-    
-    t.penup()
-    t.goto(-200,200)
-    t.pendown()
-    
-    t.color("red")
-    t.fillcolor("blue")
-    
-    t.begin_fill()
-    
-    drawTriangle(t,100)
-        
-    t.end_fill()
-    
-    t.penup()
-    t.goto(-200,-200)
-    t.pendown()
-    
-    drawHexagon(t,60)   
+    turtle.color("green")
 
+    
+    # y = f(2)
+    drawSquare(turtle, 0,0, 100, "red")
+    drawSquare(turtle, -300,-100, 50, "green")
+    
+
+    
+    turtle.penup()
+    turtle.goto(-200,200)
+    turtle.pendown()
+    
+    turtle.color("red")
+    turtle.fillcolor("blue")
+    
+    turtle.begin_fill()
+    
+    drawTriangle(turtle, 100)
+    
+    turtle.end_fill()
+    
+    turtle.penup()
+    turtle.goto(200,200)
+    turtle.pendown()
+    turtle.fillcolor("purple")
+    turtle.begin_fill()
+    drawHexagon(turtle, 50)
+    turtle.end_fill()
+    
+    
+    
+    drawPolygon(turtle, 200,-200,2,100,"grey")
+    drawPolygon(turtle, 200,200,150,4,"dark blue")
+    
     screen.exitonclick()
     
 main()
