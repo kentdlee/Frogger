c2f582b198cd8cb49bbae688668b3389ac769991
Kent Lee <kentdlee@gmail.com>
2016-03-08 22:11:39 -0600

    no message

--- draw.py
+++ draw.py
@@ -1,105 +1,143 @@
 from turtle import *
 
-def drawSquare(t, sideLength):
-    for i in range(4):
-        t.forward(sideLength)
-        t.right(90)    
-        
+# f(x) = x * x
 class Square:
-    def __init__(self, sideLength):
+    def __init__(self, x, y, sideLength, fillcolor):
+        self.x = x
+        self.y = y
         self.sideLength = sideLength
+        self.fillcolor = fillcolor
         
-    def draw(self, t):
+    def draw(self, turtle):
+        turtle.penup()
+        turtle.goto(self.x,self.y)
+        turtle.pendown()
+        turtle.fillcolor(self.fillcolor) 
+        turtle.begin_fill()    
         for i in range(4):
-            t.forward(self.sideLength)
-            t.right(90)          
+            turtle.forward(self.sideLength)
+            turtle.right(90)  
+        turtle.end_fill()        
         
 
-def drawTriangle(t, sideLength):
-    for i in range(3):
-        t.forward(sideLength)
-        t.left(120)    
-        
+def drawSquare(turtle, x, y, sideLength, fillcolor):
+    turtle.penup()
+    turtle.goto(x,y)
+    turtle.pendown()
+    turtle.fillcolor(fillcolor) 
+    turtle.begin_fill()    
+    for i in range(4):
+        turtle.forward(sideLength)
+        turtle.right(90)  
+    turtle.end_fill()
+    
 class Triangle:
     def __init__(self,sideLength):
         self.sideLength = sideLength
         
-    def draw(self,t):
+    def draw(self, turtle):
         for i in range(3):
-            t.forward(self.sideLength)
-            t.left(120)          
+            turtle.forward(self.sideLength)
+            turtle.left(120)         
         
-
-def drawHexagon(t, sideLength):
-    for i in range(6):
-        t.forward(sideLength)
-        t.left(60)    
-
-def drawPolygon(t, sideLength, numberOfSides, color):
-    t.fillcolor(color)
-    t.begin_fill()
-    for i in range(numberOfSides):
-        t.forward(sideLength)
-        angle = 360 / numberOfSides
-        t.left(angle)
-    t.end_fill()
     
+def drawTriangle(turtle, sideLength):
+    for i in range(3):
+        turtle.forward(sideLength)
+        turtle.left(120)  
+        
+def drawHexagon(turtle, sideLength):
+    for i in range(6):
+        turtle.forward(sideLength)
+        turtle.left(60)
+       
+
 class Polygon:
-    def __init__(self, sideLength, numberOfSides, color):
+    def __init__(self,x, y, sideLength, numberOfSides, fillcolor):
+        self.x = x
+        self.y = y
         self.sideLength = sideLength
         self.numberOfSides = numberOfSides
-        self. color = color
+        self.fillcolor = fillcolor
         
-    def draw(self, t):
-        t.fillcolor(self.color)
-        t.begin_fill()
+    def draw(self, turtle):
+        turtle.penup()
+        turtle.goto(self.x,self.y)
+        turtle.pendown()
+        
+        turtle.fillcolor(self.fillcolor)
+        turtle.begin_fill()
+        angle = 360 / self.numberOfSides
         for i in range(self.numberOfSides):
-            t.forward(self.sideLength)
-            angle = 360 / self.numberOfSides
-            t.left(angle)
-        t.end_fill()        
+            turtle.forward(self.sideLength)
         
+            turtle.left(angle)
+        turtle.end_fill()         
+        
+def drawPolygon(turtle, x, y, sideLength, numberOfSides, fillcolor):
+    turtle.penup()
+    turtle.goto(x,y)
+    turtle.pendown()
     
+    turtle.fillcolor(fillcolor)
+    turtle.begin_fill()
+    angle = 360 / numberOfSides
+    for i in range(numberOfSides):
+        turtle.forward(sideLength)
+    
+        turtle.left(angle)
+    turtle.end_fill()    
     
 def main():
-    t = Turtle()
-    screen = t.getscreen()
-    screen.tracer(0)
+    turtle = Turtle()
+    screen = turtle.getscreen()
     
-    #drawPolygon(t, 1, 200, "pink")
-    poly = Polygon(1, 200, "pink")
-    poly.draw(t)
-    
-    t.penup()
-    t.goto(100,200)
-    t.pendown()
-    poly.draw(t)
-    #drawSquare(t,100)   
-    sq = Square(100)
-    sq.draw(t)
-    
-    t.penup()
-    t.goto(-200,200)
-    t.pendown()
-    
-    t.color("red")
-    t.fillcolor("blue")
-    
-    t.begin_fill()
-    
-    #drawTriangle(t,100)
-    tri = Triangle(100)
-    tri.draw(t)
-        
-    t.end_fill()
-    
-    t.penup()
-    t.goto(-200,-200)
-    t.pendown()
-    
-    drawHexagon(t,60)   
+    turtle.color("green")
 
-    screen.update()
+    
+    # y = f(2)
+    #drawSquare(turtle, 0,0, 100, "red")
+    sq = Square(0,0,100,"red")
+    sq.draw(turtle)
+    #drawSquare(turtle, -300,-100, 50, "green")
+    sq2 = Square(-300,-100,50,"green")
+    sq2.draw(turtle)
+    
+
+    
+    turtle.penup()
+    turtle.goto(-200,200)
+    turtle.pendown()
+    
+    turtle.color("red")
+    turtle.fillcolor("blue")
+    
+    turtle.begin_fill()
+    
+    #drawTriangle(turtle, 100)
+    tri = Triangle(100)
+    tri.draw(turtle)
+    
+    turtle.end_fill()
+    
+    turtle.penup()
+    turtle.goto(200,200)
+    turtle.pendown()
+    turtle.fillcolor("purple")
+    turtle.begin_fill()
+    drawHexagon(turtle, 50)
+    turtle.end_fill()
+    
+    
+    
+    #drawPolygon(turtle, 200,-200,2,100,"grey")
+    #drawPolygon(turtle, 200,200,150,4,"dark blue")
+    
+    p1 = Polygon(200,-200,2,100,"grey")
+    p2 = Polygon(200,200,150,4,"dark blue")
+    p1.draw(turtle)
+    p2.draw(turtle)
+    
     screen.exitonclick()
     
 main()
